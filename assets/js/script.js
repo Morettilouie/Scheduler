ea5c80a0d8752c09b5c300b06c1ca08dc08b53b3
Louie Moretti <morettilouie@gmail.com>
2022-10-12 07:50:39 -0500

    timeblocks in html, adding classes to timeblocks in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,39 @@
-function createTimeBlocks() {
-    var createTimeBlockDiv = document.createElement("div");
-    createTimeBlockDiv.setAttribute("class", "row time-block");
-    var createHourDiv = document.createElement("div");
-    createHourDiv.setAttribute("class", "col-2 hour");
-    createHourDiv.textContent = "hour";
-    var createDescriptionDiv = document.createElement("div");
-    createDescriptionDiv.setAttribute("class", "col-8 description");
-    createDescriptionDiv.textContent = "description"
-    var createSaveDiv = document.createElement("div");
-    createSaveDiv.setAttribute("class", "col-2 save");
-    createSaveDiv.textContent = "save";
-    var container = document.querySelector(".container");
-    container.appendChild(createTimeBlockDiv);
-    createTimeBlockDiv.appendChild(createHourDiv);
-    createTimeBlockDiv.appendChild(createDescriptionDiv);
-    createTimeBlockDiv.appendChild(createSaveDiv);
-}
-createTimeBlocks()
+var todaysDate = moment().format('dddd, MMM Do YYYY');
+$('#currentDay').html(todaysDate);
+
+$(document).ready(function () {
+    $('.save-button').on('click', function () {
+        //get values of description
+        var text = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
+
+        // save in local storage
+        localStorage.setItem(time, text);
+    })
+
+    function time() {
+        // get current hour
+        var now = moment().hour();
+
+        $('.time-block').each(function () {
+            var schedule = parseInt('id').split('hour')[1];
+
+            // cycle color coordinators
+            if (schedule < now) {
+                $(this).removeClass('future');
+                $(this).removeClass('present');
+                $(this).addClass('past');
+            }
+            else if (schedule === now) {
+                $(this).removeClass('past');
+                $(this).removeClass('future');
+                $(this).addClass('present');
+            }
+            else {
+                $(this).removeClass('present');
+                $(this).removeClass('past');
+                $(this).addClass('future');
+            }
+        })
+    }
+})
